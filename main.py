0ece0006980cb2417f6de565274a3b4983d50729
karim <jundiak@gmail.com>
2024-10-10 00:29:59 -0400

    got as basic implementation of this cli to work

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv, find_dotenv
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
+
 load_dotenv(find_dotenv())
 
 uri=os.environ.get("MONGODB_URI")
 client=MongoClient(uri)
-
 
 db=client['main']
 minutesCollection = db.minutes
@@ -30,6 +30,7 @@ def updateSecondsCollection(string, collection):
             "seconds": 1
         }
         return collection.insert_one(add_document).inserted_id
+
 def updateMinutesCollection(applications, collection):
     seconds_cursor = secondsCollection.find({}, {'seconds': 1, '_id': 0})
     total_seconds = [doc['seconds'] for doc in seconds_cursor]
@@ -53,12 +54,14 @@ def updateMinutesCollection(applications, collection):
             "totalSeconds": total_seconds
         }
         return collection.insert_one(add_document).inserted_id
+
 def second():
     currentWindow = GetWindowText(GetForegroundWindow())
     print("seconds", currentWindow)
     doc = updateSecondsCollection(currentWindow, collection=secondsCollection)
     time.sleep(1)
     return doc
+
 def minute():
     minutesCollection = db.minutes
     applications = []
@@ -70,10 +73,35 @@ def minute():
     print(applications)
         
     return
+
 def hour():
-    return    
+    return
+
+def update_application_usage(collection, application_name, duration):
+    # Convert timedelta to seconds
+    duration_seconds = int(duration.total_seconds())
+    
+    collection.update_one(
+        {"application": application_name},
+        {
+            "$inc": {"usage_time": duration_seconds},
+            "$setOnInsert": {"application": application_name}
+        },
+        upsert=True
+    )
+
+def track_application_usage(duration=timedelta(minutes=1)):
+    start_time = datetime.now()
+    end_time = start_time + duration
+    
+    while datetime.now() < end_time:
+        current_window = GetWindowText(GetForegroundWindow())
+        update_application_usage(db.application_usage, current_window, timedelta(seconds=1))
+        time.sleep(1)
+
 def main():
-    minute()
-    print("python main function")
+    while True:
+        track_application_usage()
+
 if __name__ == '__main__':
     main()
